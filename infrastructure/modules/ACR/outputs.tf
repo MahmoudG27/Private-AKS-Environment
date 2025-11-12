9b805a67424d80c5501b1d136bb4d0d990b6baf7
Mahmoud Gamal <mgamal@klayytech.com>
2025-11-12 13:53:52 +0200

    First Commit

---
+++ infrastructure/modules/ACR/outputs.tf
@@ -0,0 +1,4 @@
+# Azure Container Registry id
+output "acr_id" {
+  value = azurerm_container_registry.rabbit_acr.id
+}
---
+++ infrastructure/modules/DNS/variables.tf
@@ -0,0 +1,8 @@
+variable "resource_group_name" {}
+variable "location" {}
+
+variable "aks_identity_id" {}
+
+variable "environment" {
+  type = string
+}
